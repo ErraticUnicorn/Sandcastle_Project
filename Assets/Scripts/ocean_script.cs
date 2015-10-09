d9b3a0019188a321216bd303332e792c96e541a2
Erratic Unicorn <david.j.wert@gmail.com>
2015-10-09 16:27:18 -0400

    Adds basic tide mechanic and health mechanic

---
+++ Assets/Scripts/ocean_script.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ocean_script : MonoBehaviour {
+
+	public float speed = -.01f;
+	public float tideDistance = 1;
+
+	private float xPos;
+	private float yPos;
+	private float yMax;
+	private float topBorder;
+	private bool highTide;
+	// Use this for initialization
+	void Start () {
+		highTide = true;
+		float dist = (transform.position - Camera.main.transform.position).z;
+		topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0,dist)).y;
+		topBorder = 16;
+		yMax = transform.position.y - tideDistance;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xPos = transform.position.x;
+		yPos = transform.position.y;
+		transform.position = new Vector2(xPos, yPos + speed);
+
+		if (transform.position.y < yMax && highTide ) {
+			speed = speed * -1;
+			highTide = false;
+			yMax = yPos - tideDistance;
+		}
+
+		if (transform.position.y >= topBorder && !highTide) {
+			speed = speed * -1;
+			highTide = true;
+		}
+	
+	}
+}
