2867cae2352d7880bd1019c632d85d48be4b2ead
Erratic Unicorn <david.j.wert@gmail.com>
2016-01-08 15:31:26 -0500

    Makes project 3D

--- Assets/Scripts/digging_script.cs
+++ Assets/Scripts/digging_script.cs
@@ -18,9 +18,12 @@ public class digging_script : MonoBehaviour {
 		xRes = tData.heightmapWidth;
 		yRes = tData.heightmapHeight;
 
-		randomizePoints(-0.1f);
+		Debug.Log (xRes + "," + yRes);
+		
+		initializePoints();
 	}
 
+	//120 200 -420 10
 	
 	void randomizePoints(float strength) { 
 		heights = tData.GetHeights(0, 0, xRes, yRes);
@@ -33,10 +36,36 @@ public class digging_script : MonoBehaviour {
 		
 		tData.SetHeights(0, 0, heights);
 	}
-
+	
 	void OnMouseDown() {
-		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-		Debug.Log (mousePosition.x + "," + mousePosition.y + "," + mousePosition.z);
+
+		Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.z);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Debug.Log (ray);
+		Debug.Log (Input.mousePosition);
+
+		for (int z = (int) ray.origin.z - 50; z < (int) ray.origin.z + 50; z++) {
+			for(int x = (int) ray.origin.x - 50; x < (int) ray.origin.x + 50; x++) {
+				heights[z,x] = .1f;
+			}
+		}
+
+		//heights[(int) ray.origin.z, (int) ray.origin.x] = 0.1f;
+
+		tData.SetHeights (0, 0, heights);
+
+	}
+
+	void initializePoints() {
+		heights = tData.GetHeights(0, 0, xRes, yRes);
+		
+		for (int y = 0; y < yRes; y++) {
+			for (int x = 0; x < xRes; x++) {
+				heights[x,y] = 0.0f;
+			}
+		}
+		
+		tData.SetHeights(0, 0, heights);
 	}
 	
 	// Update is called once per frame
