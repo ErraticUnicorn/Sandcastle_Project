c0af682644d9cd9a9d5ccd5ebff1507f2a5b6081
Erratic Unicorn <david.j.wert@gmail.com>
2016-01-15 16:21:43 -0500

    Adds basic water molecules

---
+++ Assets/Water Flow Free/Scripts/water_spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class water_spawner : MonoBehaviour {
+
+	public Transform waterMolecule;
+	public int instanceCount = 100;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < instanceCount; i++) {
+			Instantiate (waterMolecule, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void CreateInstances() {
+
+
+	}
+}
