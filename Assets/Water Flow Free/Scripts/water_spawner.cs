fc5675672bd76e1ed15d36e08c1072b2cbc827cd
Chielo Zimmerman <chielo@coshx.com>
2016-02-12 16:46:48 -0500

    adds smokey water molecule

--- Assets/Water Flow Free/Scripts/water_spawner.cs
+++ Assets/Water Flow Free/Scripts/water_spawner.cs
@@ -5,6 +5,7 @@ public class water_spawner : MonoBehaviour {
 
 	public Transform waterMolecule;
 	public int instanceCount = 100;
+	public int updateCount = 0;
 
 
 	// Use this for initialization
@@ -13,8 +14,11 @@ public class water_spawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < 2; i++) {
-			Instantiate (waterMolecule, new Vector3(350+Random.Range (-4, 4), 400+Random.Range (-4, 4), 100+Random.Range (-4, 4)), Quaternion.identity);
+		updateCount++;
+
+		if(updateCount %1 ==0){
+		//for (int i = 0; i < 2; i++) {
+ 		   Instantiate (waterMolecule, new Vector3(350+Random.Range (-4, 4), 200+Random.Range (-4, 4), 100+Random.Range (-4, 4)), Quaternion.identity);
 		}
 	}
 
